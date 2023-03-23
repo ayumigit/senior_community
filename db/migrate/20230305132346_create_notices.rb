5034ed6eae47b0d751d201300b14a4f8fe61e840
EC2 Default User <ec2-user@ip-172-31-37-17.ap-northeast-1.compute.internal>
2023-03-23 11:10:35 +0000

    db fixed

--- db/migrate/20230305132346_create_notices.rb
+++ db/migrate/20230305132346_create_notices.rb
@@ -1,12 +1,12 @@
 class CreateNotices < ActiveRecord::Migration[6.1]
   def change
     create_table :notices do |t|
-      t.integer :senior_id, null: false, default: ""
-      t.integer :genre_id, null:false, default: ""
-      t.string :title, null: false, default: ""
+      t.integer :senior_id, null: false
+      t.integer :genre_id, null:false
+      t.string :title, null: false
       t.text :body, null: false, default: ""
-      t.datetime :start_datetime, null: false, default: ""
-      t.datetime :end_datetime, null: false, default: ""
+      t.datetime :start_datetime, null: false
+      t.datetime :end_datetime, null: false
       t.string :place, null: false, default: ""
       t.float :rate
       t.timestamps
